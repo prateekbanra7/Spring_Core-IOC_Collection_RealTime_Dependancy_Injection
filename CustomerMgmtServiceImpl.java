c21034b354cf466dcf3804fb0830681c9bae3e5c
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-05-19 16:01:07 +0530

    Add files via upload

---
+++ CustomerMgmtServiceImpl.java
@@ -0,0 +1,45 @@
+package in.abc.service;
+
+import in.abc.bo.CustomerBO;
+import in.abc.dao.ICustomerDAO;
+import in.abc.dto.CustomerDTO;
+
+public class CustomerMgmtServiceImpl implements ICustomerMgmtService {
+
+	private ICustomerDAO dao;
+
+	static {
+		System.out.println("CustomerMgmtServiceImpl.class file is loading...");
+	}
+
+	public CustomerMgmtServiceImpl(ICustomerDAO dao) {
+		System.out.println("CustomerMgmtServiceImpl :: one param constructor...");
+		this.dao = dao;
+	}
+
+	@Override
+	public String calculateSimpleInterest(CustomerDTO dto) {
+
+		float intrAmt = 0.0f;
+
+		intrAmt = (dto.getPamt() * dto.getTime() * dto.getRate()) / 100.0f;
+
+		CustomerBO bo = new CustomerBO();
+
+		bo.setCustomerAddress(dto.getCustomerAddress());
+		bo.setCustomerName(dto.getCustomerName());
+		bo.setPamt(dto.getPamt());
+		bo.setRate(dto.getRate());
+		bo.setTime(dto.getTime());
+		bo.setInterestAmt(intrAmt);
+
+		int count = dao.insert(bo);
+		// process the result
+		if (count == 0)
+			return "Customer Registration failed :: " + dto.getPamt() + " Interest :: " + intrAmt;
+		else
+			return "Customer Registration Successfull :: " + dto.getPamt() + " Interest :: " + intrAmt;
+
+	}
+
+}
